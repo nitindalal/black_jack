622291095f9cc15ab68cf9a62a4982713b92e0ad
nitindalal <eternalnitin@gmail.com>
2016-05-08 02:54:14 +0530

    fixed points

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,6 +4,6 @@ class Card < ActiveRecord::Base
 
 	belongs_to :user
 
-	as_enum :denomination, ace: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, ten: 10, jack: 11, queen: 12, king: 13
+	as_enum :denomination, ace: 11, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, ten: 10, jack: 10, queen: 10, king: 10
 	as_enum :suit, diamond: 1, clubs: 2, hearts: 3, spade: 4
 end
--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ class Game
 	attr_accessor :cards, :dealer, :player 
 
 	#TODO change this later to initialize players first. or better ask for player name when initialized it called
-	def initialize(card_sets=4)
+	def initialize(card_sets=6)
 
 		# TODO add gem to allow multiple attributes
 		@dealer = User.new
@@ -18,9 +18,15 @@ class Game
 
 		# TODO: find a better place to do it
 		deal_first_cards
+		print_player_details
 
 		winner = hit
+		print_player_details
 		puts winner.name + " has won the game with " + winner.score.to_s + ' points'
+
+		puts "do you want to play again? Y/N"
+		player_response = get.chomp().strip().capitalize
+
 	end
 	
 	def initialize_new_card_sets(number)
@@ -44,8 +50,6 @@ class Game
 	end
 
 	def hit
-		puts @player.name + ' score - ' + @player.score.to_s
-		puts @dealer.name + ' score - ' + @dealer.score.to_s
 
 		deal @player
 		case @player.score
@@ -54,8 +58,8 @@ class Game
 		when 21..infinity
 			return @dealer
 		when 1..20
-			puts @player.name + ' score - ' + @player.score.to_s
-			puts @dealer.name + ' score - ' + @dealer.score.to_s
+			print_player_details
+
 			puts "What do you want to do? Hit or Stand?. H/S"
 			player_response = gets.chomp().strip.capitalize
 			if player_response == 'H'
@@ -70,8 +74,6 @@ class Game
 	end
 
 	def stand
-		puts @player.name + ' score - ' + @player.score.to_s
-		puts @dealer.name + ' score - ' + @dealer.score.to_s
 
 		deal @dealer
 		case @dealer.score
@@ -80,10 +82,11 @@ class Game
 		when 21..infinity
 			return @player
 		when 1..16
+			print_player_details
 			stand
 		when 17..infinity
 			# TODO ask what happens when they are equal
-			return @player.score >= @dealer.score ? @player : @dealer
+			return (@player.score >= @dealer.score ? @player : @dealer)
 		end
 	end
 
@@ -104,5 +107,11 @@ class Game
 
 	def deal player
 		player.add_card get_random_card
+		puts 'remaining_cards - ' + remaining_cards.count.to_s
+	end
+
+	def print_player_details
+		puts @player.name + " has score of #{@player.score.to_s} from cards- " + @player.cards.map{|card| card.denomination_cd.to_s + '-' + card.suit.to_s + '-' + card.card_set_id.to_s}.join(',')
+		puts @dealer.name + " has score of #{@dealer.score.to_s} from cards- " + @dealer.cards.map{|card| card.denomination_cd.to_s + '-' + card.suit.to_s + '-' + card.card_set_id.to_s}.join(',')
 	end
 end
